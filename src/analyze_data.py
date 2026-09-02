2208b40547651f5e65fb732037738c34d1ee0fd1
João Quintão <171181599+Joao-Q@users.noreply.github.com>
2026-09-02 10:44:50 +0100

    Add laboratory data quality validation

--- src/analyze_data.py
+++ src/analyze_data.py
@@ -19,3 +19,29 @@ df.info()
 
 print("\n Statistical summary:")
 print(df.describe())
+
+print("\nData validation:")
+
+print("\nCount null values:")
+print(df.isna().sum())
+
+print("\nCount duplicate rows:")
+print(df.duplicated().sum())
+
+print("\nVerify if there is duplicate IDs:")
+print(df["Sample_ID"].duplicated().sum())
+
+print("\nShow the unique values of Sample type, Test, Unit and Analyzer:")
+print("Sample type:", df["Sample_Type"].unique())
+print("Test:", df["Test"].unique())
+print("Unit:", df["Unit"].unique())
+print("Analyzer:", df["Analyzer"].unique())
+
+print("\nData type check:")
+df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
+print(df["Date"].dtype)
+
+print("\n Count invalid date: ")
+print(df["Date"].isna().sum())
+
+
